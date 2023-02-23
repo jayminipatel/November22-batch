83ed355852ce2fe5d7bf9eeec6e649e2c02d85b0
jaymini patel <jaymini_bms@yahoo.co.uk>
2023-02-23 09:45:55 +0000

    nov

---
+++ java/A/firstpack/FirstClass.java
@@ -0,0 +1,16 @@
+package A.firstpack;
+
+
+
+public class FirstClass {
+
+    public static void main(String[] args){
+
+
+    String printmanualtopic= "Manual Topics";
+        System.out.println(printmanualtopic);
+
+    }
+
+}
+
